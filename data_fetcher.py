4d02d6e3b1b608269dfa37517428b602aa5a059b
Sayed <sydr38734@gmail.com>
2024-02-07 14:59:51 +0200

    Finished dataFetcher & ReminderMessage class

--- data_fetcher.py
+++ data_fetcher.py
@@ -2,20 +2,26 @@
 Name:           Data Fetcher
 Purpose:        A script to fetch requested data from google spreadsheet
 Author:         Sayed Reda
-Last edited:    4/6/2024
+Last edited:    7/2/2024
 """
 
-import datetime
 import time
+import datetime
 import gspread
+import whatsapp
 from google.oauth2.service_account import Credentials
 
+# Spreadsheet opening scopes
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-
+# Cols numbers for each property
+studentCol, courseCol, startDateCol, endDateCol, phoneCol = 1, 3, 10, 11, 13
 
 class ReminderMessage:
     """
         A class representing each message or reminder
+
+        Input:      studentName, course, startDate, endDate, phoneNumber
+        Output:     ReminderMessage object
     """
     def __init__(self, student, course: str, startDate: str, endDate: str, phone: str):
         self.student = student
@@ -25,23 +31,26 @@ class ReminderMessage:
         self.phone = phone
     
     def value(self) -> str:
+        """
+            Returns the message value to be sent
+        """
         return f"""Assalamu Alaikum
 We'd like to remind you that *{self.student}'s* current course  ended Alhamdulilah and we're begining the next one for *{datetime.date.today().strftime("%B")}*
-_New course details:_
-*Student: * {self.student}
+
+_*New course details:*_
+*Student:* {self.student}
 *Course:* {self.course}
 *Start Date:* {self.startDate}
-*End Date: * {self.endDate}
+*End Date:* {self.endDate}
         """
 
-    def sendMessage(self) -> None:
-        pass
+    def send(self, sender: whatsapp.WASession) -> None:
+        sender.sendGroupMessage(self.phone, self.value())
 
 
 def openSpreadSheet(url: str, scopes: list[str]) -> gspread.spreadsheet:
     """
         Opens a spreadsheet of the given link and return the object
-
     """
     # Authentication
     creds = Credentials.from_service_account_file("account-credentials.json", scopes=scopes)
@@ -55,6 +64,9 @@ def openSpreadSheet(url: str, scopes: list[str]) -> gspread.spreadsheet:
 def isToday(date_str: str):
     """
         Checks if the given date string in "DD/MM/YYYY" format is today.
+
+        Input:      date string
+        Output:     True if the date is today, False otherwise
     """
     try:
         # Parse the date string
@@ -71,38 +83,82 @@ def isToday(date_str: str):
         return False
 
 
-def fetchData(spreadsheeturl: str, wsheetName: str) -> list[ReminderMessage]:
-    global SCOPES
+def fetchData(spreadsheeturl: str, wsheetName: str, range: range) -> list[ReminderMessage]:
+    """
+        Fetches the students data from given sheet 
+        which their course start date is today
 
-    # Opening worksheet with given scopes
+        Input:      spreadsheet URL, worksheet name, range of rows
+        Output:     Array of ReminderMessage objects ready to be sent
+    """
+    global SCOPES, studentCol, courseCol, startDateCol, endDateCol, phoneCol
+
+    # Opening spreadsheet
     print("Opening spread sheet...")
     ssheet = openSpreadSheet(spreadsheeturl, SCOPES)
+
+    # Opening worksheet
+    print(f"Opening {wsheetName} worksheet...")
     wsheet = ssheet.worksheet(wsheetName)
 
     # Fetching data
     print("Fetching data...")
     data: list[ReminderMessage] = list()
 
-    for row in range(2, wsheet.row_count):
-        try:
-            startDate = wsheet.cell(row, 10).value
-            if startDate is not None and isToday(startDate):
-                name, course, endDate, phone = (
-                    wsheet.cell(row, 1).value, wsheet.cell(row, 3).value,
-                    wsheet.cell(row, 11).value, wsheet.cell(row, 13).value)
-                data.append(ReminderMessage(name, course, startDate, endDate, phone))
+    for row in range:
+        checked = False
 
-        # Handling going out of Quota
-        except gspread.exceptions.APIError:
-            print("Exceeded Quota :), freezing for 60 sec...")
-            time.sleep(60)
+        # Try until current value is checked out
+        while not checked:
+            try:
+                startDate = wsheet.cell(row, startDateCol).value
+                if startDate is not None and isToday(startDate):
+                    name, course, endDate, phone = (
+                        wsheet.cell(row, studentCol).value, wsheet.cell(row, courseCol).value,
+                        wsheet.cell(row, endDateCol).value, wsheet.cell(row, phoneCol).value)
+                    data.append(ReminderMessage(name, course, startDate, endDate, phone))
+                
+                # Mark as done
+                checked = True
+
+            # Handling Quota exceed
+            except gspread.exceptions.APIError as error:
+                    if error.args[0]["code"] == 429:
+                        print("Quota exceeded, waiting for a min...")
+                        time.sleep(60)
+                    else:
+                        raise error
     
     # Return
     return data
 
 
 if __name__ == "__main__":
-    data = fetchData("https://docs.google.com/spreadsheets/d/1jQuoxYS6utICkmwbWg3e-pu7y8-i7cDu-9_sbAzmBx0/", "Sheet1")
-    for message in data:
-        print(message.value())
-        print("*"*50)
+    # Handle everything here...
+    try:
+        data = fetchData("https://docs.google.com/spreadsheets/d/1jQuoxYS6utICkmwbWg3e-pu7y8-i7cDu-9_sbAzmBx0/",
+                          "Sheet1", range(2, 58))
+        for message in data:
+            print(message.value())
+            print("*"*50)
+
+    # Credentials not found
+    except FileNotFoundError:
+        print("App credentials not found")
+    
+    # Invalid spreadsheet url
+    except gspread.exceptions.SpreadsheetNotFound:
+        print("Invalid URL")
+
+    # Invalid Worksheet Name
+    except gspread.exceptions.WorksheetNotFound:
+        print("Invalid worksheet name")
+
+    # Invalid col or row num
+    except gspread.exceptions.APIError as error:
+        print("Invalid row or column input")
+        print(f"Message: {error.args[0]['message']}")
+
+    # Otherwise
+    except:
+        print("Unexpected error")
